34a7cc008d6f130036cc849695f5d549a6588167
Arvin Khanian <arvin@xdigital.se>
2024-12-16 19:05:36 +0200

    changed structure for go handlers, css, some small changes.

--- main.go
+++ main.go
@@ -2,28 +2,21 @@ package main
 
 import (
 	"fmt"
+	"groupie-tracker/handlers"
 	"html/template"
 	"net/http"
 )
 
 var tpl *template.Template
 
-
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "home", nil)
-}
-
-
 func main() {
 
+	tpl = template.Must(template.ParseGlob("templates/*.html"))
 
- tpl = template.Must(template.ParseGlob("templates/*.html"))
-
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/", handlers.HomeHandler)
 
 	fmt.Printf("Server is running on http://localhost:10000")
 
-	 http.ListenAndServe(":10000", nil)
+	http.ListenAndServe(":10000", nil)
 
 }
