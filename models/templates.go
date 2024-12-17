b6da0aaa11ee93b0e6c5543c3ec2626e963ab5a9
Eliza John <112093038+ejmilli@users.noreply.github.com>
2024-12-17 15:08:49 +0200

    took alot of effort to make it work :)

--- models/templates.go
+++ models/templates.go
@@ -1,6 +1,14 @@
 package models
 
-import "html/template"
+import (
+	"html/template"
+)
 
-// Tmpl is a global variable that will hold the parsed templates
 var Tmpl *template.Template
+
+func init() {
+    
+    
+    Tmpl = template.Must(template.ParseGlob("templates/*.html"))
+   
+}
